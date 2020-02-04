5abfad9985056ca62402fa744455655e80be8912
Mohamed <mamdouni@salto-consulting.com>
2020-02-04 15:34:25 +0100

    [feat] generate token

--- routes/signup.js
+++ routes/signup.js
@@ -3,14 +3,15 @@ const express = require("express"),
   path = require("path"),
   rootDir = require("../util/path"),
   usersDB = require("../util/users"),
-  bcrypt = require("bcryptjs");
+  bcrypt = require("bcryptjs"),
+  jwt = require("jsonwebtoken");
 
 router.get("/signup", (req, res, next) => {
   return res.sendFile(path.join(rootDir, "views", "signup.html"));
 });
 
 router.post("/signup", function(request, response) {
-  let username = request.body.username;
+  let userEmail = request.body.username;
   let password = request.body.password;
   let confirm = request.body.confirm;
   if (confirm !== password) {
@@ -19,38 +20,68 @@ router.post("/signup", function(request, response) {
       message: "Password and confirm password must match"
     });
   }
-  if (username && password) {
+  if (userEmail && password) {
+    let userCredentials = { userEmail: userEmail, password: password };
     usersDB
-      .getByEmail(username)
+      .getByEmail(userEmail)
       .then(result => {
         if (result[0].length > 0) {
-          console.log(`${username} already exist the database`);
+          console.log(`${userEmail} already exist the database`);
           response.status(401).render("error", {
             docTitle: "401 page",
             message: "User already exist. Choose another email address please."
           });
+          throw new Error(
+            "User already exist. Choose another email address please."
+          );
         } else {
-          // hash the password
-          bcrypt
-            .hash(password, 12)
-            .then(function(hash) {
-              // Store hash in your password DB.
-              console.log(
-                `creating a new user : ${username} with a hash ${hash}`
-              );
-              return usersDB.createUser(username, hash);
-            })
-            .then(() => response.redirect("/login"));
+          return null;
         }
       })
+      .then(() => {
+        // hash the password
+        return bcrypt.hash(userCredentials.password, 12).then(function(hash) {
+          // Store hash in your password DB.
+          console.log(
+            `creating a new user : ${userCredentials.userEmail} with a hash ${hash}`
+          );
+          return hash;
+        });
+      })
+      .then(hash => {
+        return usersDB.createUser(userCredentials.userEmail, hash);
+      })
+      .then(() => {
+        console.log("userEmail : ", userCredentials.userEmail);
+        return generateToken(userCredentials.userEmail);
+      })
+      .then(token => {
+        console.log("the generated token is : ", token);
+      })
+      .then(() => response.redirect("/login"))
       .catch(err => console.log(err));
   } else {
     console.log("Not authorized");
     response.status(400).render("error", {
       docTitle: "400 page",
-      message: "Username and password are required "
+      message: "userEmail and password are required "
     });
   }
 });
+
+/**
+ * this method will generate a token to send to the user via an email
+ * we are using jsonwebtoken so we gonna easily generate a token and verify if it's valid
+ */
+const generateToken = userEmail => {
+  // Create a token
+  const payload = { user: userEmail };
+  const options = { expiresIn: "1d", issuer: "https://your-domain.io" };
+  const secret = process.env.JWT_SECRET;
+  console.log("secret : ", secret);
+  const token = jwt.sign(payload, secret, options);
+  console.log("token : ", token);
+  return token;
+};
 
 module.exports = router;
