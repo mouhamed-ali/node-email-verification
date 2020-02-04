930b00caba450cc6f7ade3006cfe511f0c2e76e7
Mohamed <mamdouni@salto-consulting.com>
2020-02-04 11:11:56 +0100

    [feat] activate dashbord link

--- routes/common.js
+++ routes/common.js
@@ -5,7 +5,9 @@ rootDir = require("../util/path");
 
 // in this route we will serve the common routes
 router.get("/", (req, res, next) => {
-  return res.sendFile(path.join(rootDir, "views", "index.html"));
+  //return res.sendFile(path.join(rootDir, "views", "index.html"));
+  res.render("index", { docTitle: "Index page", path: "/" });
+  //we have added the path to set the active class in the main-layout.pug. check the code to understand
 });
 
 module.exports = router;
